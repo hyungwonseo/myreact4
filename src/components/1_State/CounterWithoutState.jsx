51dacf96cc1e72a487f7eedca99657b950ffb65c
micosara <micosara365@gamil.com>
2025-08-19 10:35:58 +0900

    Create CounterWithoutState.jsx

---
+++ src/components/1_State/CounterWithoutState.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+let count = 0;
+
+function CounterWithoutState() {
+    function increase() {
+        count = count + 1;
+        console.log("현재 카운트: " + count);
+    }
+  return (
+    <>
+        <h2>CounterWithoutState</h2>
+        <p> Count: {count}</p>
+        <button onClick={()=>increase()}> + </button>
+    </>
+  )
+}
+
+export default CounterWithoutState
