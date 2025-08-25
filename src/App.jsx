173f9f01e3a92062d084674b3a9b53afb16b71e4
micosara <micosara365@gamil.com>
2025-08-25 14:52:37 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import BasicRouter from "./components/4_Router/BasicRouter"
 
 function App() {
--- src/components/4_Router/BasicRouter.jsx
+++ src/components/4_Router/BasicRouter.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
 import Button from "../3_Props/Button"
+import styled from 'styled-components';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+const Menu = styled.div``
+const Body = styled.div``
 
 function BasicRouter() {
   const [view, setView] = useState("home");
@@ -7,10 +13,13 @@ function BasicRouter() {
     <>
     <h1>BasicRouter</h1>
     <Menu>
-      <Button bgcolor='dodgerblue'
-              title='HOME'
-              func={()=>setView("home")} />
+      <Button bgcolor='dodgerblue' title='HOME' func={()=>setView("home")} />
+      <Button bgcolor='dodgerblue' title='ABOUT' func={()=>setView("about")} />
+      <Button bgcolor='dodgerblue' title='CONTACT' func={()=>setView("contact")} />
     </Menu>
+    <Body>
+      { view == "home" ? <Home /> : view == "about" ? <About /> : <Contact />}
+    </Body>
     </>
   )
 }
