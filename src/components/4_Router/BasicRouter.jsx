fed74b844db6c6e47a7c8aac3a28527b6fb58b8e
micosara <micosara365@gamil.com>
2025-08-25 14:15:12 +0900

    1

---
+++ src/components/4_Router/BasicRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BasicRouter() {
+  return (
+    <div>BasicRouter</div>
+  )
+}
+
+export default BasicRouter
---
+++ src/components/4_Router/ReactRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReactRouter() {
+  return (
+    <div>ReactRouter</div>
+  )
+}
+
+export default ReactRouter
