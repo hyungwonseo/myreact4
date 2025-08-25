a8e9e0fc6e41ab9605d05cdd6f353a1bf4a8b616
micosara <micosara365@gamil.com>
2025-08-25 10:37:56 +0900

    Update Menu.jsx

--- src/components/3_Props/Menu.jsx
+++ src/components/3_Props/Menu.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+const Ul = styled.ul``
+const Li = styled.li``
+const Link = styled.a``
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <Container>
+        <Ul>
+            <Li><Link href="#">Home</Link></Li>
+            <Li><Link href="#">Content</Link></Li>
+            <Li><Link href="#">About Us</Link></Li>
+            <Li><Link href="#">Contact</Link></Li>
+            <Li><Link href="#">Social</Link></Li>
+        </Ul>
+    </Container>
   )
 }
 
