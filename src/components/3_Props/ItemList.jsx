5ff451d94d9d304e94ab95f795999c86863ca8e1
micosara <micosara365@gamil.com>
2025-08-25 11:16:27 +0900

    Update ItemList.jsx

--- src/components/3_Props/ItemList.jsx
+++ src/components/3_Props/ItemList.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ItemList() {
+  const [items, setItems] = useState([]);
+  const [newInputItem, setNewInputItem] = useState("");
+
   return (
-    <div>ItemList</div>
+    <>
+        <input placeholder='물품이름을 입력해주세요'
+            onChange={(e) => setNewInputItem(e.target.value)}
+            value={newInputItem} />
+        <button onClick={() => {
+            const temp = [...items, newInputItem];
+            setItems(temp);
+            setNewInputItem("");
+        }}>물품추가</button>
+    </>
   )
 }
 
