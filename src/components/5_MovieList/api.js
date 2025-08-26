4da901d55c6d16e219e6b29002a2df2551cbe0db
micosara <micosara365@gamil.com>
2025-08-26 15:29:25 +0900

    Update api.js

--- src/components/5_MovieList/api.js
+++ src/components/5_MovieList/api.js
@@ -6,3 +6,20 @@ const header = {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDFmN2JmMDgwOWMxZGFlNTViYzgyMTkzNDcwMTQwMiIsIm5iZiI6MTY4NDYyODMwNC43NjksInN1YiI6IjY0Njk2MzUwYTUwNDZlMDBlNWI2NjBkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o6x7t4i4LNeo5Hnt2rggEsj6GBF6pF97CzFOUXTTpLo'
   },
 };
+
+export const categories = [
+    { category: "Now Playing", func: getMoviesNowPlaying },
+    { category: "Popular", func: getMoviesPopular },
+    { category: "Top Rated", func: getMoviesTopRated },
+    { category: "Upcoming", func: getMoviesUpcoming },
+];
+
+function getMoviesNowPlaying() {
+    return axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
+        header
+    );
+}
+function getMoviesPopular() {}
+function getMoviesTopRated() {}
+function getMoviesUpcoming() {}
