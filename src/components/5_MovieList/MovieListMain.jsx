6a0e7dd3ab9d091a360776eff4dfd6f95130c736
micosara <micosara365@gamil.com>
2025-08-26 11:49:28 +0900

    Update MovieListMain.jsx

--- src/components/5_MovieList/MovieListMain.jsx
+++ src/components/5_MovieList/MovieListMain.jsx
@@ -1,4 +1,41 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./Home"
+import MovieWrapper from "./MovieWrapper"
+import MovieList from "./MovieList"
+import MovieDetail from "./MovieDetail"
+import Search from "./Search"
+import MyPage from "./MyPage"
+import Login from "./Login"
+import Navbar from "./Navbar"
+import Error from "./Error"
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+const Section = styled.div`
+  width: 60%;
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+  @media (min-width: 768px) and (max-width: 1023px) {
+    width: 80%;
+  }
+  @media (min-width: 1024px) {
+    width: 60%;
+  }
+`;
+const Menu = styled.div`
+  width: 100%;
+`;
+const ContentBox = styled.div`
+  width: 100%;
+  margin-top: 30px;
+`;
+const Footer = styled.div``;
 
 function MovieListMain() {
   return (
