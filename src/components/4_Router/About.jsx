ebc0db88ef8ef3156fbb3dad652b4dadc9fa2385
micosara <micosara365@gamil.com>
2025-08-26 09:35:48 +0900

    Update About.jsx

--- src/components/4_Router/About.jsx
+++ src/components/4_Router/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function About() {
+  const { id } = useParams();
   return (
+    <>
     <div>About</div>
+    <p>{id}</p>
+    </>
   )
 }
 
