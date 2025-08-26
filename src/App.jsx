6cfb965ed6346dea1e03cfa8d687f42fe63b5c0e
micosara <micosara365@gamil.com>
2025-08-26 12:48:26 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createGlobalStyle } from 'styled-components';
 import MovieListMain from './components/5_MovieList/MovieListMain';
+
+const GlobaStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Poppins;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
 
 function App() {
   return (
     <>
+    <GlobaStyle />
     <MovieListMain />
     </>
   )
