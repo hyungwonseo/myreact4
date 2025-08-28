086ed7dbda27f703cada8c7e0bfef28648096ff1
micosara <micosara365@gamil.com>
2025-08-28 10:05:39 +0900

    Update Search.jsx

--- src/components/5_MovieList/Search.jsx
+++ src/components/5_MovieList/Search.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { Button, Container, Card, Img, Text } from './MovieList'
+import { IMG_PATH, getGenreListMovie, getGenreName } from './api'
 
 function Search() {
   return (
