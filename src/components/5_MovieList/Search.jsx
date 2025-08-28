0734d5968f7dd5791a29823c181e4c602532e408
micosara <micosara365@gamil.com>
2025-08-28 10:09:36 +0900

    Update Search.jsx

--- src/components/5_MovieList/Search.jsx
+++ src/components/5_MovieList/Search.jsx
@@ -4,6 +4,20 @@ import styled from 'styled-components'
 import { Button, Container, Card, Img, Text } from './MovieList'
 import { IMG_PATH, getGenreListMovie, getGenreName } from './api'
 
+const SearchBox = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+`
+const Input = styled.input`
+  width: 500px;
+  height: 40px;
+`
+const H3 = styled.h3`
+  margin-bottom: 20px;
+`
+
 function Search() {
   return (
     <div>Search</div>
