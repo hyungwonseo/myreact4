85ea15bee49e124d19cb651053e41ea1b1e0aea0
micosara <micosara365@gamil.com>
2025-08-29 11:18:27 +0900

    Update CounterDisplay.jsx

--- src/components/6_Zustand/CounterDisplay.jsx
+++ src/components/6_Zustand/CounterDisplay.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useCounterStore } from './CounterWrapper'
 
 function CounterDisplay() {
+  const { count } = useCounterStore();
+  console.log("CounterDisplay 렌더링됨");
   return (
+    <>
     <div>CounterDisplay</div>
+    <p>현재 값: {count}</p>
+    </>
   )
 }
 
