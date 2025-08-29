c07170fd6808ecb1ad085d996a6423409da31865
micosara <micosara365@gamil.com>
2025-08-29 11:07:47 +0900

    Update CounterWrapper.jsx

--- src/components/6_Zustand/CounterWrapper.jsx
+++ src/components/6_Zustand/CounterWrapper.jsx
@@ -3,8 +3,8 @@ import { create } from 'zustand'
 
 export const useCounterStore = create((set) => ({
     count: 0,
-    increase: ()=>set((state)=>{count: state.count + 1}),
-    decrease: ()=>set((state)=>{count: state.count - 1}),
+    increase: ()=>set((state)=> ({count: state.count + 1})),
+    decrease: ()=>set((state)=> ({count: state.count - 1})),
 }));
 
 function CounterWrapper() {
